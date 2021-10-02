86abbcd39288f9460ffc28ce2893b37c1bb2848c
Crowdbotics <team@crowdbotics.com>
2021-10-02 16:18:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/flat_fog_31048/MainActivity.java
@@ -0,0 +1,15 @@
+package com.flat_fog_31048;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "flat_fog_31048";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "flat_fog_31048",
+  url: "https://flat_fog_31048.botics.co",
+  api: "https://flat_fog_31048.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
